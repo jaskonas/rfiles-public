c5f14a5cf70951fdc758b9c44dfc01adcd1182b9
jaskonas <jon.askonas@gmail.com>
2016-04-12 13:21:43 +0100

    Latest

--- eggerstest1.R
+++ eggerstest1.R
@@ -34,3 +34,43 @@ grepl("b[ai]n", c("banana", "binary", "bones"))
 
 # another useful thing: "abc|def" means "match abc or def"
 grepl("app|ban", vec)
+parli=read.csv("https://raw.githubusercontent.com/jaskonas/rfiles-public/master/THC_candidates.csv",stringsAsFactors=F)
+parli$oxmatch <- grepl("Oxford",parli$bio)
+summary(parli)
+head(parli$bio[parli$oxmatch])
+parli$bio[grepl("Oxford",parli$bio)]
+parli$oxccmatch = grepl("[Oo]xford City",parli$bio)
+parli$bio[parli$oxccmatch]
+#parli$ms=grepl("^Mrs|^Miss|^Ms",parli$bio)
+parli$bio[grepl("^Mrs|^Miss|^Ms",parli$bio)]
+###nextlesson
+library(tm)
+library(SnowballC)
+sw=stopwords('english')
+#can then modify. Say we are looking for being words, like in Heidegger
+#can then stem or lematize
+text=c("am","are","is") #stems seperate, but would lematize diff
+text2=c("politics","polity","political")
+text3=c("communication","communicative",'communicate')
+stemDocument(text)
+stemDocument(text2)
+stemDocument(text3)
+#lemmas are better when you can-use koRpus and treetagger
+
+#will need to pre-process metaphors, multiwords, etc.
+#there are some multiword lexemes - but no good lemmatization schemes yet
+##try out frequent ngrams (frequent combinations of 2 words, 3 words, etc) and collocations
+##How to do multiword preprocessing? substitution across the Corpus
+##synonyms - wordnetcode.princeton.edu. Download dictionaries and use term filters
+
+#concordances have now been implemented in R
+library(quanteda)
+#kwic(Corpus,"search term", window=5[where you want it displayed],wholeword=F or T) Can then view and write to a CSV
+
+#word collocations
+#Spaces of collocation - surface conoccurence, textual coocurrence, syntactic coocurrence
+
+#implemented in quanteda - collocations function. Gives it to you as observed and expected frequencies
+
+#ngrams- co-occuring words w/in a giving window. Typlical you move one word forward when comparing ngrams. (1 (one word),2,3,4 etc.). might need to do pre-processing to use right
+s="Felix struggles finishing this PhD. I am struggling finishing my PhD. I am Andy, struggling with this corpus."
