4f7240adea8f07ce4d1bd9ec77e3ad02a64d9b3b
Jon Askonas <jonaskonas@Lauras-MacBook-Pro-4.local>
2016-04-12 09:39:45 +0100

    first go

---
+++ eggerstest1.R
@@ -0,0 +1,31 @@
+vec1 = c(1,4,3,6) #one data type in one dimension
+vec1==4 #double equal is a truth test. The way this works is that it tests whether EACH element equals 4
+vec = c("apple","2 bananas", "pineapple","another fruit")
+vec == "apple"
+
+grepl("apple", vec)
+grepl("app",vec)
+
+sub("a","A",vec) #what I'm finding, what I am replacing, the data frame
+gsub("a","A",vec)##sub only does 1st - gsub does all
+vec2=sub("pineapple","pineXXX",vec)
+grepl("apple",vec2)
+grepl("^apple",vec) ##carrot sign ^ the beginning of the string"the mystery and power of regex"
+grepl("^apple", vec)
+# inside a regular expression, "^" means "the start of the string of characters."
+grepl("^a",vec)
+# give us?  1 & 4
+
+# inside a regular expression, "$" means "the end of the string of characters"
+# so how do we identify the entries that end with e? 
+grepl("e$", vec)
+
+### Some other useful metacharacters 
+# "." is the wildcard
+# \\d is a digit 
+# \\D is non-digit 
+# \\s means a space 
+# + means one or more 
+# * means zero or more 
+grepl("\\s", vec)
+grepl("\\d", vec)
